b586d44f15b7987ad7d6391a7545193418b28864
DangCongTien <dctien00@gmail.com>
2021-03-07 22:33:32 +0700

    exercise 5.1, 5.2, 5.4, 5.5, 5.6, 5.7

---
+++ src/Lab02/triangle.java
@@ -0,0 +1,25 @@
+package Lab02;
+import java.util.Scanner;
+
+public class triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Scanner s = new Scanner(System.in);
+		System.out.println("height of triangle: ");
+		n= s.nextInt();
+		do {
+		for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n-i; j++) {
+            	System.out.print("   ");
+            }
+            for(int k = 1; k <= 2*i-1; k++) {
+            	System.out.print(" * ");
+            }
+            System.out.println("");
+		}
+		}while (n<0);
+	}
+
+}
