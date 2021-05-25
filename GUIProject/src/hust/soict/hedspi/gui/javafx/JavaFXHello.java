3fa10c5c4c3b09cd9a33d426e3d0f597f466e1d6
dctien <dctien00@gmail.com>
2021-05-25 22:27:43 +0700

    upload assignment javafx of lab09

--- GUIProject/src/hust/soict/hedspi/gui/javafx/JavaFXHello.java
+++ GUIProject/src/hust/soict/hedspi/gui/javafx/JavaFXHello.java
@@ -1,5 +1,4 @@
 package hust.soict.hedspi.gui.javafx;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -8,25 +7,19 @@ import javafx.stage.Stage;
 
 public class JavaFXHello extends Application{
 	private Button btnHello;
-	
-	@Override
 	public 	void start(Stage primaryStage) {
-		// Construct the "Button" and attach an "EventHandler"
 		btnHello = new Button();
 		btnHello.setText("Say Hello");
-		// Using JDK 8 Lambda Expression to construct an EventHandler<ActionEvent>
 		btnHello.setOnAction(evt -> System.out.println("Hello World!"));
-		
-		// Construct a scene graph of nodes
 		StackPane root = new StackPane();
 		root.getChildren().add(btnHello);
-
+		
 		Scene scene = new Scene(root, 300, 100);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Hello");
 		primaryStage.show();
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
